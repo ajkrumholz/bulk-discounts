7329cc810788db9cae512fa05e6a9444b680eea6
A.J. Krumholz <krumholz.alan@gmail.com>
2022-09-25 18:13:54 -0400

    add code to prevent active discounts from being edited

--- spec/features/merchants/bulk_discounts/show_spec.rb
+++ spec/features/merchants/bulk_discounts/show_spec.rb
@@ -40,6 +40,23 @@ RSpec.describe "merchant bulk discount show page" do
 
         expect(current_path).to eq(edit_merchant_bulk_discount_path(merchant_1, discount_1))
       end
+
+      describe 'if the discount is active on any in progress invoices' do
+        it 'will display and error and stay on the show page' do
+          alaina = Customer.create!(first_name: "Alaina", last_name: "Kneiling")
+          licorice = merchant_1.items.create!(name: "Licorice Funnels", description: "Licorice Balls", unit_price: 1200)
+          invoice = alaina.invoices.create!(status: :in_progress, created_at: "2012-01-30 14:54:09")
+          invoice_item = InvoiceItem.create!(invoice_id: invoice.id, item_id: licorice.id, quantity: 5, unit_price: 2700, status:"shipped" )
+
+          visit merchant_bulk_discount_path(merchant_1, discount_1)
+
+          click_link("Edit this discount")
+
+          expect(current_path).to eq(merchant_bulk_discount_path(merchant_1, discount_1))
+
+          expect(page).to have_content("This discount is cannot be edited while active on an in-progress invoice!")
+        end
+      end
     end
   end
 end
