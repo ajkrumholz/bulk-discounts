c3ede2ae860656120f5679cf4769c910c80fbbd3
A.J. Krumholz <krumholz.alan@gmail.com>
2022-09-27 16:14:32 -0400

    move webmock stub to before each blocks in tests that involve api calls

--- spec/features/merchants/bulk_discounts/new_spec.rb
+++ spec/features/merchants/bulk_discounts/new_spec.rb
@@ -17,6 +17,13 @@ RSpec.describe 'new merchant bulk discount page' do
   }
   
   before :each do
+    @json_response = File.open("./fixtures/next_three_holidays.json") 
+    @uri = "https://date.nager.at/api/v3/NextPublicHolidays/US"
+    stub_request(:get, @uri).with(headers: {
+      'Accept'=>'*/*',
+      'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+      'User-Agent'=>'Ruby'
+      }).to_return(status: 200, body: @json_response, headers: {})
     visit new_merchant_bulk_discount_path(merchant_1)
   end
 
--- spec/features/merchants/bulk_discounts/show_spec.rb
+++ spec/features/merchants/bulk_discounts/show_spec.rb
@@ -17,6 +17,13 @@ RSpec.describe "merchant bulk discount show page" do
   }
 
   before :each do
+    @json_response = File.open("./fixtures/next_three_holidays.json") 
+    @uri = "https://date.nager.at/api/v3/NextPublicHolidays/US"
+    stub_request(:get, @uri).with(headers: {
+      'Accept'=>'*/*',
+      'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+      'User-Agent'=>'Ruby'
+      }).to_return(status: 200, body: @json_response, headers: {})
     visit merchant_bulk_discount_path(merchant_1, discount_1)
   end
 
